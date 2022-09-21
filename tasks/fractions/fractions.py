d7b41fc7e85c7940cf47b42a662a385a469cfc7a
lnkk6 <n.sunsv.6@gmail.com>
2022-09-21 12:38:31 +0300

    practice tasks

--- tasks/fractions/fractions.py
+++ tasks/fractions/fractions.py
@@ -8,6 +8,38 @@
 
 Можно считать, что передаваемые в функцию b и d всегда ненулевые.
 """
+def nok(n, m):
+    if n > m:
+        rez = n
+    else:
+        rez = m
+    while(True):
+        if rez % n == 0 and rez % m == 0:
+            return rez
+        else:
+            rez += 1
+
+def delitely(znam, chisl):
+    s = 2
+    while s <= min(abs(znam), abs(chisl)) // 2:
+        while znam % s == 0 and chisl % s == 0:
+            znam //= s
+            chisl //= s
+        s += 1
+    return chisl, znam
+
+def add_fractions(a, b, c, d):
+    znam = nok(b, d)
+    chisl = znam // b * a + znam // d * c
+    if znam < 0:
+        znam = abs(znam)
+        chisl = -chisl
+    print(delitely(znam, chisl))
+
+print('Введите 4 числа')
+a, b, c, d = int(input()), int(input()), int(input()), int(input())
+add_fractions(a, b, c, d)
+
 
 
 def add_fractions(a, b, c, d):
